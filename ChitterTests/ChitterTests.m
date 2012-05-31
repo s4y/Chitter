9d76af1c96e6651777b28232c116a3ed373790d0
Sidney San Martín and Sam Epstein <hello+Sidnicious+omni5cience@sidneysm.com>
2012-05-31 18:26:01 -0400

    That ain’t no DeepTech copyright

--- ChitterTests/ChitterTests.m
+++ ChitterTests/ChitterTests.m
@@ -3,7 +3,7 @@
 //  ChitterTests
 //
 //  Created by Sidney San Martín on 5/31/12.
-//  Copyright (c) 2012 DeepTech, Inc. All rights reserved.
+//  Copyright (c) 2012 Sam and Sidney All rights reserved.
 //
 
 #import "ChitterTests.h"
